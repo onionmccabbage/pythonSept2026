eeb6b6f4ea542d3fc94e56b54a21d0d5b83ff67a
onionmccabbage <onionmccabbage@live.com>
2026-09-01 12:31:30 +0100

    using dict

--- using_loops.py
+++ using_loops.py
@@ -6,4 +6,14 @@
 for i in range(0, 10, 2): # start, stop-before, step (can be negative)
     print(i)
 
+x = 0
+# a little guessing game
+while x != 5: # != means 'not equal to'
+    x = int(float(input('guess the number: ')))
 
+# the dictionary collection
+# a dict is a non-ordinal mutable collection of any data type as key:value pairs
+d = {'fn':'Floella', 'ln':'Benjamin', 'attrib':'Dame'} # here the {} indicate a dictionary
+d['attrib'] = 'Rt Hon'
+print(d, type(d))
+print(d['fn'])
