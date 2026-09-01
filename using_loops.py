b6a4bb28cbbdb0ea7839bc0760e96cb01c6cfe4d
onionmccabbage <onionmccabbage@live.com>
2026-09-01 12:21:20 +0100

    using range

---
+++ using_loops.py
@@ -0,0 +1,9 @@
+# There are several kinds of loop in Python
+# for loop
+# while loop
+# we will also meet 'range'
+
+for i in range(0, 10, 2): # start, stop-before, step (can be negative)
+    print(i)
+
+
