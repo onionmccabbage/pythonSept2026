282718fd8659ae17aef5091084b2b65f077119c5
onionmccabbage <onionmccabbage@live.com>
2026-09-02 11:53:37 +0100

    using generaotor comprehension

--- using_comp.py
+++ using_comp.py
@@ -9,10 +9,8 @@ r = range(0, 10**10, 10) # start, stop-before, step (step is optional defaults t
 # NB the values do not exist in memory, they are generated on demand
 c = (i**2 for i in range(0,10)) # this comprehensively deals with each member of the range to do a calculation
 
-
-
-# using generator
-
+# using generator to create in-memory values
+l = [i**3 for i in range(-10,11)] # [] always makes a list
 
 if __name__ == '__main__':
     # here we exercise the code in this module
@@ -21,3 +19,7 @@ if __name__ == '__main__':
     #     # print(_) # it is fairly common to use underscore _ as an iterator
     #     pass
     print(c, type(c))
+    for _ in c:
+        # anything raised to 0.5 is the square root
+        print(f'{_**0.5} squared is {_}')
+    print(l, type(l))
