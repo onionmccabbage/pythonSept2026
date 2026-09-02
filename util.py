32b5701f1bf5de5c54c0436319805e362db9a102
onionmccabbage <onionmccabbage@live.com>
2026-09-02 09:52:07 +0100

    a utility module

---
+++ util.py
@@ -0,0 +1,18 @@
+# a utility module
+
+def getNum():
+    'Ask the user for a number. Non-numeric entries raise an exception'
+    try:
+        # remember every input is ALWAYS a string
+        n = input('Please enter an integer: ')
+        num = int(float(n))
+        return num
+    except Exception as err:
+        print(f'The value is not a number {err}')
+        return 1 # a default number returned if the user did not enter a numeric value
+
+if __name__ == '__main__':
+    # code here will only execute if this module is run directly
+    # IE the following code will NOT run if this module is imported elsewehere
+    my_num = getNum()
+    print(my_num)
