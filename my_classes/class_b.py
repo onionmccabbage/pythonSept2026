47679068d2a5f3124fb5a1b042bcf6283e80d8d2
onionmccabbage <onionmccabbage@live.com>
2026-09-03 11:54:37 +0100

    class with two properties and a __str__ method

--- my_classes/class_b.py
+++ my_classes/class_b.py
@@ -2,6 +2,9 @@
 # name must be a non-empty string
 # age integer greater than 13
 
+# Python tends to use leading and trailing double underscores a lot
+# we see __name__, __main__, __init__, __str__ and loads more 
+# this is referred to as 'dunder' for double-underscore
 
 class Person():
     '''store a persons name ensuring it is a non-empty string
@@ -33,13 +36,20 @@ class Person():
             self.__age = new_age # name-mangle this value
         else:
             raise TypeError('Age must be an integer greater than 13')
+    # we can write our own print function to choose how our class gets printed
+    def __str__(self): # overriding the default __str__ of our class
+        return f'This is {self.name} aged {self.age}'
+        
 
 
 if __name__ == '__main__':
     #make instanves of our Person class
     Ada = Person('Ada', 99)
+    print(Ada) # this line will use our __str__ method
     Beth = Person('Beth', 14)
-    print(Ada.name)
+    print(Ada.name, Ada.age) # we can access properties of a custom class like this
+    Beth.age = 15 # this will be validated (using the setter method of our class)
+    print(Beth.name, Beth.age)
     try:
         Floella = Person(True, 66) # this will fail
     except TypeError:
